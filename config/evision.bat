d5798a015ccce7247a82ec8fff2127dc7c3018e5
Casper <draakie@protonmail.com>
2021-02-18 05:54:34 +0100

    remove evision setup

--- config/evision.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-:: eVision Cmd configuration
-set sv=%proj%\ShiftVision
-set bv=%proj%\BarrierVision
-
--- config/evision.ps1
+++
@@ -1,3 +0,0 @@
-# eVision PowerShell configuration
-$Env:sv = "$Env:proj\ShiftVision"
-$Env:bv = "$Env:proj\BarrierVision"

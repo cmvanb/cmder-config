70fc231e8529c7fc0925ab0607a508e60d597550
Casper <draakie@protonmail.com>
2021-02-18 05:26:42 +0100

    updated setup script for windows command line

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Casper's Cmder Config
 
 1. Install Cmder
-2. Run Cmder
-3. `cd scripts/`
-4. `setup-cmder-config.bat`
+2. Run regular windows cmd.exe
+3. Navigate to project scripts directory  
+	`cd cmder-config/scripts`
+4. Ensure `%cmder_root%` environment variable is defined  
+	`set cmder_root=<INSERT_CMDER_ROOT_PATH>`
+5. Run the setup script  
+	`setup-cmder-config.bat`
 
 # Note
 
--- scripts/setup-cmder-config.bat
+++ scripts/setup-cmder-config.bat
@@ -1,13 +1,13 @@
 @echo off
 
 :: remove recursively any files already in config dir
-rmdir /S /Q "%cmder_root%\config"
+rmdir /s /q "%cmder_root%\config"
 
 :: make a junction (symbolic link directory) for config/
-mklink /J "%cmder_root%\config" "%~dp0..\config"
+mklink /j "%cmder_root%\config" "%~dp0..\config"
 
 :: remove current version of ConEmu.xml
-rm "%cmder_root%\vendor\conemu-maximus5\ConEmu.xml"
+del /q "%cmder_root%\vendor\conemu-maximus5\ConEmu.xml"
 
 :: make a symbolic link for ConEmu.xml
 mklink "%cmder_root%\vendor\conemu-maximus5\ConEmu.xml" "%~dp0..\vendor\conemu-maximus5\ConEmu.xml"
